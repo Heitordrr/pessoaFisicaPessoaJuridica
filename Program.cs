d141374a634fa9617c0375311a12c4db8cfb23b5
Heitordrr <heitorhendo@hotmail.com>
2022-10-06 18:06:58 -0300

    Foi implementado o endereçamento da pessoaFisica e pessoaJuridica

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ PessoaFisica novapf = new PessoaFisica ();
 
 novapf.nome = "Heitor";
 novapf.cpf = "12345678910";
-
-
+novapf.endereco = "Belém, Pará, Brasil";
 //Console.WriteLine(novapf.nome);
 //Console.WriteLine(novapf.cpf);
+Console.WriteLine("Bem vindo " + novapf.nome + ", seu cpf é: " + novapf.cpf + " e seu endereço é " + novapf.endereco);
 
-Console.WriteLine("Bem vindo " + novapf.nome + ", seu cpf é: " + novapf.cpf);
+Console.WriteLine("--------------------------------------------------------------------------------------------");
+
+PessoaJuridica novapj = new PessoaJuridica ();
+
+novapj.razaoSocial = "HOMEM LTDA";
+novapj.cnpj = "02.464.694/0001-43";
+novapj.endereco = "São Paulo/sn";
+
+Console.WriteLine("Nome da empresa é " + novapj.razaoSocial + ", seu CNPJ é: " + novapj.cnpj + " e seu endereço é " + novapj.endereco);
+
+
